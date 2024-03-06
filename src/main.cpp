a99b6fdc34d88c5849c1400fbb8c40bfaa1fa463
DAVID GENOTELLE <david.genotelle@gmail.com>
2024-03-06 18:19:08 +0100

    //code eric

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,51 @@
-#include <Arduino.h>
+// Blinks an LED attached to a MCP23XXX pin.
 
-// put function declarations here:
-int myFunction(int, int);
+// ok to include only the one needed
+// both included here to make things simple for example
+//#include <Adafruit_MCP23X08.h>
+#include <Adafruit_MCP23X17.h>
+
+#define LED_PIN 0     // MCP23XXX pin LED is attached to
+#define BUTTON_PIN 1
+
+// only used for SPI
+#define CS_PIN 4
+
+// uncomment appropriate line
+//Adafruit_MCP23X08 mcp;
+Adafruit_MCP23X17 mcp;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  //test
+  Serial.begin(115200);
+  //while (!Serial);
+  Serial.println("MCP23xxx Blink Test!");
+
+  // uncomment appropriate mcp.begin
+  //if (!mcp.begin_I2C()) {
+  if (!mcp.begin_SPI(CS_PIN)) {
+    Serial.println("Error.");
+    while (1);
+  }
+
+  // configure pin for output
+  mcp.pinMode(LED_PIN, OUTPUT);
+
+  // configure button pin for input with pull up
+  mcp.pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  Serial.println("Looping...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  /*
+  mcp.digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  mcp.digitalWrite(LED_PIN, LOW);
+  delay(500);
+*/
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // LOW = pressed, HIGH = not pressed
+  mcp.digitalWrite(LED_PIN, !mcp.digitalRead(BUTTON_PIN));
+  
+
 }
